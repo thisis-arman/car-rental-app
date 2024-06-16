902283467f743e9889f070532ffec8873d9d8268
Arman Hossen <armanhossen1549@gmail.com>
2024-06-17 00:31:30 +0600

    booking service

---
+++ src/app/modules/Booking/booking.controller.ts
@@ -0,0 +1,62 @@
+import httpStatus from "http-status";
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { CarServices } from "./car.service";
+
+const addNewCar = catchAsync(async (req, res, next) => {
+  const result = CarServices.addNewCarIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: "Car added successfully",
+    success: true,
+    data: result,
+  });
+});
+
+const getAllCars = catchAsync(async (req, res, next) => {
+  const result = CarServices.getAllCarsFromDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Cars are retrieved successfully",
+    data: result,
+  });
+});
+
+const getSingleCar = catchAsync(async (req, res, next) => {
+  const result = CarServices.getSingleCarFromDB(req.params.carId);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Car is retrieved successfully",
+    data: result,
+  });
+});
+
+const updateSingleCar = catchAsync(async (req, res, next) => {
+  const result = CarServices.updateCarFromDB(req.params.carId, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Car is updated successfully",
+    data: result,
+  });
+});
+
+const deleteSingleCar = catchAsync(async (req, res, next) => {
+  const result = CarServices.deleteCarFromDB(req.params.carId);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Car is deleted successfully",
+    data: result,
+  });
+});
+
+export const bookingControllers = {
+  addNewBooking,
+  getAllBookings,
+  getSingleBooking,
+  updateSingleBooking,
+  deleteSingleBooking,
+};
---
+++ src/app/modules/Booking/booking.service.ts
@@ -0,0 +1,33 @@
+import { TBooking } from "./booking.interface";
+import Booking from "./booking.model";
+
+const createBookingIntoDB = async (payload: TBooking) => {
+  const result = await Booking.create(payload);
+  return result;
+};
+
+const getAllBookingsFromDB = async (query: Record<string, unknown>) => {
+  const result = await Booking.find(query);
+  return result;
+};
+
+const getSingleBookingFromDB = async (_id: string) => {
+  const result = await Booking.findById(_id);
+  return result;
+};
+const updateBookingFromDB = async (_id: string, payload: Partial<TBooking>) => {
+  const result = await Booking.findByIdAndUpdate(_id, { payload });
+  return result;
+};
+const deleteBookingFromDB = async (_id: string) => {
+  const result = await Booking.findByIdAndUpdate(_id, { isDeleted: true });
+  return result;
+};
+
+export const CarServices = {
+  createBookingIntoDB,
+  getAllBookingsFromDB,
+  getSingleBookingFromDB,
+  updateBookingFromDB,
+  deleteBookingFromDB,
+};
