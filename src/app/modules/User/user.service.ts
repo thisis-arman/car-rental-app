5440c41a72a93db898a2625057cb4640d81991e9
Arman Hossen <armanhossen1549@gmail.com>
2024-06-16 22:36:08 +0600

    user services

---
+++ src/app/modules/User/user.service.ts
@@ -0,0 +1,52 @@
+import { TUser } from "./user.interface";
+import { User } from "./user.model";
+
+
+
+// TODO:implement transition and rollback here in this function
+const createUserIntoDB = async (payload: TUser) => {
+  const user = await User.create(payload);
+  return user;
+};
+
+const getAllUsersFromDB = async (query: Record<string, unknown>) => {
+  const user = await User.find(query);
+  return user;
+};
+
+const getSingleUserFromDB = async (_id: string) => {
+  const user = await User.findById(_id);
+  return user;
+};
+
+//TODO:implement Dynamic update in this function
+const updateUserIntoDB = async (_id: string, payload: Partial<TUser>) => {
+  const user = await User.findByIdAndUpdate(
+    _id,
+    {
+      payload,
+    },
+    {
+      $upsert: true,
+        runValidators: true,
+        new: true
+    }
+  );
+  return user;
+};
+
+// TODO:implement transition and rollback here in this function
+const deleteUserFromDB = async (_id: string,) => {
+    const deletedUser = await User.findByIdAndUpdate(_id, {
+        isDeleted: true
+    })
+
+    return deletedUser;
+}
+
+export const userServices = {
+  createUserIntoDB,
+  getAllUsersFromDB,
+  getSingleUserFromDB,
+  updateUserIntoDB,
+};
