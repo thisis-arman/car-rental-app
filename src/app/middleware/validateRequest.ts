f6b65f8d97728b39ca59fca23d90e43477e7dcf4
Arman Hossen <armanhossen1549@gmail.com>
2024-06-16 19:57:40 +0600

    validate request

---
+++ src/app/middleware/validateRequest.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject } from "zod";
+import catchAsync from "../utils/catchAsync";
+
+export const validateRequest = (schema: AnyZodObject) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    await schema.parseAsync({
+      body: req.body,
+    });
+    next();
+  });
+};
